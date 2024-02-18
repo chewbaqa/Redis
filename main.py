752832840979d9774234e32c82880bf738ee68b2
Vishal <vx-shal@users.noreply.github.com>
2024-02-18 16:33:59 +0530

    Responds to multiple pings

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept()
     with conn:
-        while(message:=conn.recv) == True:
-            conn.sendall(ping.encode())
+        while True:
+            conn.recv(1024)
+            conn.sendall(ping.encode)
 
 
 if __name__ == "__main__":
