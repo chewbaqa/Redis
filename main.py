6d2763f90ba714b4e0b1afae244a67832fd9e1c2
Vishal <vx-shal@users.noreply.github.com>
2024-02-18 16:43:05 +0530

    support for concurrent clients

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import socket
+import threading
 
 
-def main():
-
+def handle_client(conn):
     ping = "+PONG\r\n"
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()
     with conn:
         while True:
             conn.recv(1024)
-            conn.sendall(ping.encode)
+            conn.sendall(ping.encode())
+
+
+def main():
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    while True:
+        conn, addr = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(conn,))
+        client_thread.start()
 
 
 if __name__ == "__main__":
